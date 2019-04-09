a9fa7edf487574cf4fed3aade70e766920ad0f76
zdazx <zhangduo0227@163.com>
2019-04-09 11:03:44 +0800

    practice03

--- src/main/java/practice03/Person.java
+++ src/main/java/practice03/Person.java
@@ -1,4 +1,28 @@
 package practice03;
 
 public class Person {
+    private String name;
+    private int age;
+
+    Person(){}
+
+    Person(String name,int age){
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String introduce(){
+        String intro = "My name is " +
+                getName() + ". I am "+
+                getAge() + " years old.";
+        return intro;
+    }
 }
--- src/main/java/practice03/Worker.java
+++ src/main/java/practice03/Worker.java
@@ -1,4 +1,13 @@
 package practice03;
 
-public class Worker {
+public class Worker extends Person {
+
+    Worker(String name, int age){
+        super(name, age);
+    }
+
+    @Override
+    public String introduce() {
+        return "I am a Worker. I have a job.";
+    }
 }
