cca73e60ad6f285b8bd55a246aaa0333a7425c8e
zdazx <zhangduo0227@163.com>
2019-04-09 10:45:44 +0800

    practice01

--- src/main/java/practice01/Person.java
+++ src/main/java/practice01/Person.java
@@ -1,4 +1,26 @@
 package practice01;
 
 public class Person {
+    private String name;
+    private int age;
+
+    Person(String name,int age){
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String introduce(){
+        String intro = "My name is " +
+                getName() + ". I am "+
+                getAge() + " years old.";
+        return intro;
+    }
 }
