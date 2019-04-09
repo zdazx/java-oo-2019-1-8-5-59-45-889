e72dc5462d63f5d35558d36ebab8a4ca32b7afe5
zdazx <zhangduo0227@163.com>
2019-04-09 11:39:44 +0800

    practice06

--- src/main/java/practice06/Person.java
+++ src/main/java/practice06/Person.java
@@ -1,4 +1,28 @@
 package practice06;
 
 public class Person {
+    private String name;
+    private int age;
+
+    Person(){}
+
+    Person(String name,int age){
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String introduce(){
+        String intro = "My name is " +
+                getName() + ". I am "+
+                getAge() + " years old.";
+        return intro;
+    }
 }
--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,26 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person {
+    private int klass;
+
+    Teacher(String name, int age){
+        super(name, age);
+    }
+
+    Teacher(String name, int age, int klass){
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+
+    @Override
+    public String introduce() {
+        return super.introduce() +
+                (klass == 0 ?
+                        " I am a Teacher. I teach No Class." :
+                        " I am a Teacher. I teach Class "+getKlass()+".");
+    }
 }
