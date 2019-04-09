107d59d668bbf751bd11ed921101405377198be7
zdazx <zhangduo0227@163.com>
2019-04-09 12:02:59 +0800

    practice07

--- src/main/java/practice07/Student.java
+++ src/main/java/practice07/Student.java
@@ -1,4 +1,20 @@
 package practice07;
 
-public class Student {
+public class Student extends Person {
+    private Klass klass;
+
+    Student(String name, int age, Klass klass){
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    @Override
+    public String introduce() {
+        return super.introduce() + " I am a Student. I am at " +
+                getKlass().getDisplayName() +".";
+    }
 }
--- src/main/java/practice07/Teacher.java
+++ src/main/java/practice07/Teacher.java
@@ -1,4 +1,36 @@
 package practice07;
 
-public class Teacher {
+public class Teacher extends Person {
+    private Klass klass;
+
+    Teacher(String name, int age){
+        super(name, age);
+    }
+
+    Teacher(String name, int age, Klass klass){
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    @Override
+    public String introduce() {
+        return super.introduce() +
+                (klass == null ?
+                        " I am a Teacher. I teach No Class." :
+                        " I am a Teacher. I teach "
+                                +getKlass().getDisplayName()+".");
+    }
+
+    public String introduceWith(Student student){
+        return super.introduce() + " I am a Teacher. " +
+                (
+                        (student.getKlass().getNumber() == this.getKlass().getNumber()) ?
+                                "I teach " :
+                                "I don't teach "
+                ) + student.getName() + ".";
+    }
 }
